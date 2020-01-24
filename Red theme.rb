2ed3cdac41781b91a533fcb32add0143a3ffdd2d
mgallardo1 <45017805+mgallardo1@users.noreply.github.com>
2020-01-24 12:04:31 -0800

    Add files via upload

---
+++ Red theme.rb
@@ -0,0 +1,310 @@
+# Welcome to Sonic Pi v3.1
+charizard = "C:/Users/miguel_gallardo/Documents/Audacity/Charizard.wav"
+use_bpm 194
+use_synth :blade
+sample charizard
+sleep 17.54
+play :D4
+play :A3
+play :D3
+sleep 2
+play :D4
+play :A3
+play :D3
+sleep 2
+play :D4
+play :A3
+play :D3
+sleep 2
+play :D4
+play :A3
+play :D3
+sleep 1
+play :D4
+play :B3
+play :D3
+play :E2
+sleep 1
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :D4
+play :D3
+sleep 1.5
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :D4
+play :A3
+play :D3
+sleep 1.5
+play :D4
+play :A3
+play :D3
+play :D2
+sleep 0.5
+play :D4
+play :A3
+play :D3
+sleep 1.5
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :C3
+play :D4
+play :B3
+sleep 1
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :D4
+play :A3
+play :D3
+sleep 1.5
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :D4
+play :A3
+play :D3
+sleep 1.5
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :D4
+play :A3
+play :D3
+sleep 1.5
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :D4
+play :D3
+sleep 0.5
+play :D4
+play :B3
+sleep 1
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :D4
+play :D3
+sleep 1.5
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :D4
+play :A3
+play :D3
+sleep 1.5
+play :D3
+play :A3
+sleep 0.5
+play :D4
+play :A3
+play :D3
+sleep 1.5
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :D4
+play :A3
+play :D3
+sleep 0.5
+play :C5
+play :B3
+sleep 1
+live_loop :song do
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :A3
+  sleep 0.5
+  play :A4
+  play :D4
+  play :A3
+  sleep 1
+  play :D5
+  play :A4
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D5
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :B4
+  play :D4
+  play :B3
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :D3
+  sleep 0.5
+  play :A4
+  play :D4
+  play :A3
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :D3
+  sleep 0.5
+  play :B3
+  play :D3
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :A4
+  play :D4
+  play :A3
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :B4
+  play :B3
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :A4
+  play :D4
+  play :A3
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :D3
+  sleep 0.5
+  play :F4
+  play :D4
+  play :G3
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :A4
+  play :D4
+  play :A3
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :D3
+  sleep 0.5
+  play :B4
+  play :D4
+  play :B3
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :A4
+  play :D4
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :B3
+  play :D3
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :D4
+  play :A3
+  play :D3
+  sleep 0.5
+  play :F4
+  play :C5
+  play :B3
+  sleep 1
+  play :D4
+  play :A3
+  play :D3
+  play :A3
+  sleep 0.5
+  play :D4
+  play :A3
+  play :D3
+  play :A3
+  sleep 0.5
+  play :F4
+  play :C5
+  play :C4
+  sleep 1
+  play :D5
+  play :A4
+  play :D4
+  sleep 2
+  play :D4
+  play :D3
+  play :A3
+  sleep 2
+  play :E5
+  play :B4
+  play :B3
+  sleep 1
+end
+
+
